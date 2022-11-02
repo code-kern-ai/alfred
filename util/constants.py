97bf84b2b22ee596ae0b252eea59850ff5211a5d
Felix Kirsch <40427105+FelixKirsch@users.noreply.github.com>
2022-11-02 17:12:31 +0100

    Moves docker compose template to refinery (#2)
    * moves docker compose template to refinery
    
    * change version in replace release script

--- util/constants.py
+++ util/constants.py
@@ -2,7 +2,7 @@
 BACKUP_DIR = "/refinery/backup"
 CONNECTION_STRING = "postgresql://postgres:onetask@graphql-postgres:5432"
 DOCKER_COMPOSE = "/refinery/docker-compose.yml"
-DOCKER_COMPOSE_TEMPLATE = "templates/docker-compose.yml"
+DOCKER_COMPOSE_TEMPLATE = "/refinery/docker-compose.tmpl"
 EXEC_ENVS = {
     "AC_EXEC_ENV": "kernai/refinery-ac-exec-env",
     "ML_EXEC_ENV": "kernai/refinery-ml-exec-env",
